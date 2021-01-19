214a08d17e84e7200daac419d9a0c3f58baac7dc
ivlnwn <ivlnwn@gmail.com>
2021-01-20 00:23:55 +0700

    fix

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export {makeFetch} from './src/api/core';
-export {getClearAction, getSetExtraDataAction} from './src/reducer/actions';
+export * from './src/reducer/actions';
 export * from './src/reducer/reducer';
